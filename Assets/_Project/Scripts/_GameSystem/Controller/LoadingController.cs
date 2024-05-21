6f8bc48fe99f973f6a41b0cee23948a9b2dcd91a
HoangVanThu99 <guardian.of.gods99@gmail.com>
2024-05-21 14:08:21 +0700

    -update

--- Assets/_Project/Scripts/_GameSystem/Controller/LoadingController.cs
+++ Assets/_Project/Scripts/_GameSystem/Controller/LoadingController.cs
@@ -1,3 +1,4 @@
+using System;
 using CustomTween;
 using TMPro;
 using UnityEngine;
@@ -6,26 +7,32 @@ using UnityEngine.UI;
 
 public class LoadingController : MonoBehaviour
 {
-    [Header("Attributes")] 
+    [Header("Attributes")]
     [SerializeField] private float timeLoading = 5f;
-    
-    [Header("Components")]
+
+    [Header("Components")] 
+    [SerializeField] private Image titleFrame;
+    [SerializeField] private TextMeshProUGUI title;
+    [SerializeField] private Image logo;
+    [SerializeField] private Image background;
     [SerializeField] private Image progressBar;
     [SerializeField] private TextMeshProUGUI loadingText;
     private AsyncOperation _sceneOperation;
 
     void Start()
     {
-        #if UNITY_EDITOR
-        timeLoading = 0f;
-        #endif
-        
-        
         _sceneOperation = SceneManager.LoadSceneAsync("GameplayScene");
         _sceneOperation.allowSceneActivation = false;
         
         progressBar.fillAmount = 0;
-        Tween.UIFillAmount(progressBar,1, timeLoading).OnUpdate(loadingText, (loadingText, tween) => loadingText.text = $"Loading {(int) (progressBar.fillAmount * 100)}%").OnComplete(()=>
+        Tween.Alpha(titleFrame, 1, timeLoading);
+        Tween.Alpha(title, 1, timeLoading);
+        Tween.Alpha(logo, 1, timeLoading);
+        Tween.Alpha(background, 1, timeLoading);
+        Tween.UIFillAmount(progressBar,1, timeLoading).OnUpdate(loadingText, (loadingText, tween) =>
+        {
+            loadingText.text = $"Loading {(int) (progressBar.fillAmount * 100)}%";
+        }).OnComplete(()=>
         {
             _sceneOperation.allowSceneActivation = true;
         });
